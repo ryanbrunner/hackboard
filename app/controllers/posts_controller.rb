b5147045d5ab74f3cee00f21b767946c29b99149
Ryan Brunner <ryan@ryanbrunner.com>
2013-03-04 20:42:23 -0500

    Create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,17 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
 end
