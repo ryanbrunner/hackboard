2e89544ea7c0e6efee8a504880f9221605d26db0
Ryan Brunner <ryan@ryanbrunner.com>
2013-03-04 20:03:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackboard::Application.config.secret_token = '906b594c47375e11418a942e595c7b8f87c3e6b729e0d34e31c4e9f465a1535594edb059c664899c1b98cd65b9775fd5b4f79d1788c513d5ee3755bf82b02074'
